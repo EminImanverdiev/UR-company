c5dae2020de5f71dc3735489150fbbc28f47f330
Emin <111564141+Elminn007@users.noreply.github.com>
2023-06-10 14:26:29 +0400

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,27 @@
+
+$(window).scroll(function() {
+    if($(this).scrollTop()>126){
+     $('#header .navbar').addClass('active');
+     $('#header .navbar .nav-item .nav-link').addClass('active');
+     $('#header .navbar .nav-item .nav-link').addClass('otheractive');
+    }
+    else{
+        $('#header .navbar').removeClass('active');
+        $('#header .navbar .nav-item .nav-link').removeClass('active');
+    }
+});
+$("#header .navbar").on("mouseleave",function() {
+    $("#header .navbar .nav-item").removeClass("item-hover");
+})
+$("#header .navbar .nav-item").on("mouseover",function(){
+$("#header .navbar .nav-item").addClass("item-hover");
+$(this).removeClass("item-hover");
+})
+document.querySelector('.contact-button').addEventListener('click',function() {
+   document.querySelector('#contact').classList.add('active')   
+   document.querySelector('body').classList.add('active');
+})
+document.querySelector('#close-contact').addEventListener('click',function() {
+   document.querySelector('body').classList.remove('active')    
+   document.querySelector('.contact-section').classList.remove('active')    
+ })
